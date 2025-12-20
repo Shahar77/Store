c0468ec983b78351f5b948f12aacd7b07855f56c
Shahar77 <shaharezra3@gmail.com>
2025-12-21 01:09:23 +0200

    Enhance BookProduct with author and pages details
    Added author and pages attributes to BookProduct class with corresponding methods.

--- src/store/products/BookProduct.java
+++ src/store/products/BookProduct.java
@@ -8,13 +8,23 @@ import java.awt.Color;
 
 /**
  * Represents a book product.
+ * Includes author name and number of pages.
  */
 public class BookProduct extends Product{
+
     private String author;
     private int pages;
 
     /**
      * Creates a new book product.
+     * @param name book title
+     * @param price book price
+     * @param stock amount in stock
+     * @param description book description
+     * @param color display color
+     * @param imagePath path to image
+     * @param author book author
+     * @param pages number of pages
      */
     public BookProduct(String name,double price,int stock,String description,Color color,String imagePath,String author,int pages){
         super(name,price,stock,description,Category.BOOKS,color,imagePath);
@@ -23,11 +33,25 @@ public class BookProduct extends Product{
     }
 
     /**
-     * Returns detailed book info.
+     * @return book author
+     */
+    public String getAuthor(){
+        return author;
+    }
+
+    /**
+     * @return number of pages
+     */
+    public int getPages(){
+        return pages;
+    }
+
+    /**
+     * @return detailed book description
      */
     @Override
-    public String getDisplayDetails(){
-        return super.getDisplayDetails()+
+    public String toString(){
+        return super.toString()+
                "\nAuthor: "+author+
                "\nPages: "+pages;
     }
