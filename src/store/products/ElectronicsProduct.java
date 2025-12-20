1917181a3cf0570772214831d8ff5aa37f48ac6d
Shahar77 <shaharezra3@gmail.com>
2025-12-21 01:10:58 +0200

    Refactor ElectronicsProduct class documentation and methods

--- src/store/products/ElectronicsProduct.java
+++ src/store/products/ElectronicsProduct.java
@@ -7,14 +7,24 @@ package store.products;
 import java.awt.Color;
 
 /**
- * Represents an electronic product.
+ * Represents an electronics product.
+ * Includes warranty duration and brand.
  */
 public class ElectronicsProduct extends Product{
+
     private int warrantyMonths;
     private String brand;
 
     /**
      * Creates a new electronics product.
+     * @param name product name
+     * @param price product price
+     * @param stock amount in stock
+     * @param description product description
+     * @param color display color
+     * @param imagePath path to image
+     * @param warrantyMonths warranty duration in months
+     * @param brand product brand
      */
     public ElectronicsProduct(String name,double price,int stock,String description,Color color,String imagePath,int warrantyMonths,String brand){
         super(name,price,stock,description,Category.ELECTRONICS,color,imagePath);
@@ -23,12 +33,26 @@ public class ElectronicsProduct extends Product{
     }
 
     /**
-     * Returns detailed electronics info.
+     * @return warranty duration in months
+     */
+    public int getWarrantyMonths(){
+        return warrantyMonths;
+    }
+
+    /**
+     * @return product brand
+     */
+    public String getBrand(){
+        return brand;
+    }
+
+    /**
+     * @return detailed electronics description
      */
     @Override
-    public String getDisplayDetails(){
-        return super.getDisplayDetails()+
-               "\nBrand: "+brand+
-               "\nWarranty: "+warrantyMonths+" months";
+    public String toString(){
+        return super.toString()+
+               "\nWarranty Months: "+warrantyMonths+
+               "\nBrand: "+brand;
     }
 }
