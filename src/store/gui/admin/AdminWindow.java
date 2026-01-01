e15ecd19628e82c6592dd9edf3d093eef44989c0
Shahar77 <shaharezra3@gmail.com>
2026-01-01 21:50:37 +0200

    Update AdminWindow.java

--- src/store/gui/admin/AdminWindow.java
+++ src/store/gui/admin/AdminWindow.java
@@ -1,3 +1,8 @@
+/**
+ * Submitted by:
+ * Sarah Gabay - ID 329185771
+ * Shahar Ezra - ID 329186118
+ */
 package store.gui.admin;
 
 import store.engine.StoreEngine;
@@ -5,15 +10,31 @@ import store.gui.admin.controller.AdminController;
 
 import javax.swing.*;
 
+/**
+ * Main admin window of the application.
+ * Acts as the top-level frame for administrative operations.
+ */
 public class AdminWindow extends JFrame {
 
+    /**
+     * Shared store engine.
+     */
     private final StoreEngine engine;
+
+    /**
+     * Admin controller.
+     */
     private final AdminController controller;
 
+    /**
+     * Creates a new AdminWindow.
+     *
+     * @param engine store engine instance
+     */
     public AdminWindow(StoreEngine engine) {
         super("Admin");
-        this.engine = engine;
-        this.controller = new AdminController(engine);
+        this.engine=engine;
+        this.controller=new AdminController(engine);
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setContentPane(new AdminPanel(controller));
