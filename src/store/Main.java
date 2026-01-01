5d4d0bffa658d20fd5c7aa974fe30ad4869cfbea
Shahar77 <shaharezra3@gmail.com>
2026-01-01 21:59:18 +0200

    Update Main.java

--- src/store/Main.java
+++ src/store/Main.java
@@ -1,3 +1,8 @@
+/**
+ * Submitted by:
+ * Sarah Gabay - ID 329185771
+ * Shahar Ezra - ID 329186118
+ */
 package store;
 
 import store.engine.StoreEngine;
@@ -10,19 +15,33 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Application entry point.
+ * Initializes the store engine, seeds initial products
+ * and launches the GUI.
+ */
 public class Main {
 
+    /**
+     * Main method of the application.
+     *
+     * @param args command-line arguments
+     */
     public static void main(String[] args) {
-
-        StoreEngine engine = new StoreEngine();
+        StoreEngine engine=new StoreEngine();
         seedProducts(engine);
 
-        LauncherWindow w = new LauncherWindow(engine);
+        LauncherWindow w=new LauncherWindow(engine);
         w.setVisible(true);
     }
 
+    /**
+     * Seeds the store with initial products.
+     *
+     * @param engine store engine instance
+     */
     private static void seedProducts(StoreEngine engine) {
-        List<Product> products = new ArrayList<>();
+        List<Product> products=new ArrayList<>();
 
         products.add(new SimpleProduct(
                 "T-Shirt",
