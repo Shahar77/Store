0665777ba125868288aa40ff836ae5744d81b959
Shahar77 <shaharezra3@gmail.com>
2026-01-01 21:54:17 +0200

    Update CustomerRunnable.java

--- src/store/gui/customer/thread/CustomerRunnable.java
+++ src/store/gui/customer/thread/CustomerRunnable.java
@@ -1,3 +1,8 @@
+/**
+ * Submitted by:
+ * Sarah Gabay - ID 329185771
+ * Shahar Ezra - ID 329186118
+ */
 package store.gui.customer.thread;
 
 import store.engine.StoreEngine;
@@ -5,18 +10,33 @@ import store.gui.launcher.LauncherWindow;
 
 import javax.swing.*;
 
+/**
+ * Runnable responsible for launching the customer GUI.
+ * Ensures the launcher window is created on the Swing Event Dispatch Thread.
+ */
 public class CustomerRunnable implements Runnable {
 
+    /**
+     * Shared store engine.
+     */
     private final StoreEngine engine;
 
+    /**
+     * Creates a new CustomerRunnable.
+     *
+     * @param engine store engine instance
+     */
     public CustomerRunnable(StoreEngine engine) {
-        this.engine = engine;
+        this.engine=engine;
     }
 
+    /**
+     * Runs the customer launcher initialization.
+     */
     @Override
     public void run() {
         SwingUtilities.invokeLater(() -> {
-            LauncherWindow w = new LauncherWindow(engine);
+            LauncherWindow w=new LauncherWindow(engine);
             w.setVisible(true);
         });
     }
